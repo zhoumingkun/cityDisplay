bd3b72384c82dec25f664c2ff4b555a8bc77151b
sanhaoqingnian <su2426694419@qq.com>
2019-11-29 17:03:03 +0800

    //新增处警单表的注入

--- src/main/java/com/toughguy/cityDisplay/controller/content/CJDBController.java
+++ src/main/java/com/toughguy/cityDisplay/controller/content/CJDBController.java
@@ -1,10 +1,14 @@
 package com.toughguy.cityDisplay.controller.content;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.toughguy.cityDisplay.service.content.prototype.ICJDBService;
 
 @RestController
 @RequestMapping("/CJDB")
 public class CJDBController {
-
+	@Autowired
+	private ICJDBService cjdbServiceImpl;
 }
--- src/main/java/com/toughguy/cityDisplay/service/content/impl/CJDBServiceImpl.java
+++ src/main/java/com/toughguy/cityDisplay/service/content/impl/CJDBServiceImpl.java
@@ -1,12 +1,15 @@
 package com.toughguy.cityDisplay.service.content.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.toughguy.cityDisplay.model.content.CJDB;
+import com.toughguy.cityDisplay.persist.content.prototype.ICJDBDao;
 import com.toughguy.cityDisplay.service.content.prototype.ICJDBService;
 import com.toughguy.cityDisplay.service.impl.GenericServiceImpl;
 
 @Service
 public class CJDBServiceImpl extends GenericServiceImpl<CJDB, Integer> implements ICJDBService {
-
+	@Autowired
+	private ICJDBDao cjdbDaoImpl;
 }
